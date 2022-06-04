d678ad67b4e8ace8ec534f1039f68ca0c9874989
inhohwangg <101077035+inhohwangg@users.noreply.github.com>
2022-06-05 03:23:59 +0900

    test

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ const io = new Server(server, {
     }
 })
 
+//ㅎㅇ
 io.on('connection', (socket) => {
-    console.log(`${socket.id} 입장`)
+    console.log(`${socket.id}입장`)
     console.log('소켓 연결되었습니다.')
     // socket.on('join_room', (data)=> {
     //     socket.join(data)
