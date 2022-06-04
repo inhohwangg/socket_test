54652978a98c6fcb0e9645930462ee63836ed8ca
inhohwangg <101077035+inhohwangg@users.noreply.github.com>
2022-06-04 23:53:37 +0900

    socket 수정

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = require('express')()
 const http = require('http')
 const {Server} = require('socket.io')
 const cors = require('cors')
-const { SocketAddress } = require('net')
 const server = http.createServer(app)
 
 app.use(cors())
