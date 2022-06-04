b6e52127267fcc50ab7227e2d237e8351853479d
inhohwangg <101077035+inhohwangg@users.noreply.github.com>
2022-06-04 23:27:31 +0900

    socket 수정

---
+++ app.js
@@ -0,0 +1,33 @@
+const app = require('express')()
+const http = require('http')
+const {Server} = require('socket.io')
+const cors = require('cors')
+const { SocketAddress } = require('net')
+const server = http.createServer(app)
+
+app.use(cors())
+
+const io = new Server(server, {
+    cors : {
+        origin : "http://localhost:3000",
+        methods:["GET,POST"]
+    }
+})
+
+io.on('connection', socket => {
+
+    console.log('소켓 연결되었습니다.')
+
+    socket.on('send_message',(data)=> {
+        socket.emit('receive_message', data)
+    })
+
+    socket.on('disconnect', ()=> {
+        console.log('연결이 끊어졌습니다.')
+    })
+})
+
+
+http.listen(3000, function(){
+    console.log('3000포트로 서버가 실행되었습니다.')
+})
