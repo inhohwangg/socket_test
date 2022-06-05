65b89be04895a430af34a1ecc023283ad87c7b9b
inhohwangg <101077035+inhohwangg@users.noreply.github.com>
2022-06-05 21:36:10 +0900

    test

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ io.on("connection", (socket)=> {
 
     socket.on("send_message", (data)=> {
         socket.to(data.room).emit("receive_message", data)
-        console.log(data)
+        console.log(socket.id, data)
     })
 })
 
